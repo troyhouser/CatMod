ccfb06cf5286c07be7c83559be6066fb308c15da
troyhouser <122118639+troyhouser@users.noreply.github.com>
2024-07-01 18:58:15 -0700

    Update weight_dim_dist.R

--- R/weight_dim_dist.R
+++ R/weight_dim_dist.R
@@ -1,3 +1,11 @@
 weight_dim_dist = function(exp_dim_dist,attention_weights){
-  attention_weights * exp_dim_dist
+  N = dim(exp_dim_dist)[3]
+  weighted_dist = array(dim = c(dim(exp_dim_dist)))
+  for(i in 1:N){
+    for(j in 1:nrow(exp_dim_dist)){
+      weighted_dist[j,,i] = exp_dim_dist[j,,i] * attention_weights
+    }
+  }
+  weighted_dist
 }
+
