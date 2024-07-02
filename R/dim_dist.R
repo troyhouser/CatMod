4126e07b8f51022989995ee7291b6a166f500f1b
troyhouser <122118639+troyhouser@users.noreply.github.com>
2024-07-01 17:09:06 -0700

    Add files via upload

--- R/dim_dist.R
+++ R/dim_dist.R
@@ -1,5 +1,5 @@
 dim_dist = function(category_representations,stimuli){
-  stimuli = t(as.matrix(stimuli))
+  stimuli = as.matrix(stimuli)
   dist = array(dim = c(nrow(stimuli),ncol(stimuli),nrow(category_representations)))
   for(i in 1:nrow(stimuli)){
     for(j in 1:nrow(category_representations)){
